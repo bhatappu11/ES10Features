2dbdbbefdb53418212fd4c540256ac50459873d3
Arpitha Bhat <arpithabht12@gmail.com>
2021-12-13 09:14:19 +0530

    [Arpitha]ADDED: demonstration of symbol description

---
+++ SymbolDescription.js
@@ -0,0 +1,6 @@
+var mySym = 'Hello'
+var symObj = Symbol(mySym)
+
+console.log(symObj)
+console.log(String(symObj))
+console.log(symObj.description)
---
+++ ToString.js
@@ -0,0 +1,7 @@
+function print(msg){
+    var message = msg;
+    console.log(`hello ${message}`)
+}
+console.log(print.toString())
+
+print('Appu');
